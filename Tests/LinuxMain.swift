c2bc32f4b4fe80195c8a4afe7c981c08bb049f8a
Corey McMahon <corey.d.mcmahon@gmail.com>
2020-04-15 20:40:58 -0500

    add tests to linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,10 @@
 import XCTest
 
 import PredicateTests
+import PredicateCollectionsTests
 
 var tests = [XCTestCaseEntry]()
 tests += PredicateTests.__allTests()
+tests += PredicateCollectionsTests.__allTests()
 
 XCTMain(tests)
