e0b6c26f1e60a65f60bcd07715181aa63c936a40
Corey McMahon <corey.d.mcmahon@gmail.com>
2020-04-15 21:27:12 -0500

    Revert "try new availability check"
    This reverts commit 328086a89dbe9f41939e9c79d05dfcbb6e807ee1.

--- Tests/PredicateTests/PredicateTests.swift
+++ Tests/PredicateTests/PredicateTests.swift
@@ -14,4 +14,10 @@ final class PredicateTests: XCTestCase {
     XCTAssertFalse(PredicateTests.isEven.test(1))
     XCTAssertFalse(PredicateTests.isOdd.test(2))
   }
+
+  @available(swift 5.2)
+  func testCallableValue() {
+    XCTAssertTrue(PredicateTests.isOdd(1))
+    XCTAssertFalse(PredicateTests.isEven(1))
+  }
 }
